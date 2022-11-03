38c257d55856422789c1a1b09c40e16c3adb741d
ivanaMesterLightIt <ivana.mester@lightit.io>
2022-11-03 11:28:18 -0300

    progress class 18

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@styles/Header.scss';
-
-import menu from '@icons/icon_menu.svg';
+import Menu from '@components/menu';
+import menuIcon from '@icons/icon_menu.svg';
 import logo from '@logos/logo_yard_sale.svg'
 import shoppingCart from '@icons/icon_shopping_cart.svg'
 
 const Header = () => {
+    const [toggle, setToggle] = useState(false);
+    const handleToggle = () => {
+        setToggle(!toggle);
+    }
     return (
         <nav>
-            <img src={menu} alt="menu" className="menu" />
+            <img src={menuIcon} alt="menu" className="menu" />
             <div className="navbar-left">
             <img src={logo} alt="logo" className="nav-logo" />
             <ul>
@@ -34,13 +38,16 @@ const Header = () => {
             </div>
             <div className="navbar-right">
             <ul>
-                <li className="navbar-email">platzi@example.com</li>
+                <li className="navbar-email" onClick={handleToggle}>
+                    platzi@example.com
+                </li>
                 <li className="navbar-shopping-cart">
                 <img src={shoppingCart} alt="shopping cart" />
                 <div>2</div>
                 </li>
             </ul>
             </div>
+            {toggle && <Menu/>}
         </nav>
     );
 }
